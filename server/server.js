e73817cd5a2519aa8879041c437792560282f6bb
retnodamayanti <mayahere5@gmail.com>
2023-08-21 21:34:06 +0930

    import schemas and added graphql route

--- server/server.js
+++ server/server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/connection'); // Import the database connection function
+const { ApolloServer } = require('apollo-server-express');
+const connectDB = require('./config/connection'); 
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: ({ req }) => {
+        // Add your context logic if needed
+      },
+    });
+
+    server.start().then(() => {
+      server.applyMiddleware({ app });
+
+      // Add a route for testing the GraphQL endpoint
+      app.get('/graphql', (req, res) => {
+        res.send('GraphQL endpoint is running.');
+      });
+
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      });
     });
   })
   .catch(error => console.error('Error connecting to MongoDB:', error));
@@ -19,10 +38,3 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Server is running.');
 });
-
-// Add more routes and middleware as needed
-
-// // Set up GraphQL endpoint
-// app.use('/graphql', graphqlHTTP({
-//   // Your GraphQL configuration here
-// }));
