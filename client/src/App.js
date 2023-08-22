60a2fbf139a16e9a59a22351c8ff02a5afbb1987
retnodamayanti <mayahere5@gmail.com>
2023-08-22 17:35:18 +0930

    added login_mutation and auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ApolloProvider } from '@apollo/client';
+import ApolloClient from './utils/ApolloClient'; 
+import Auth from './components/Auth';
+import Dashboard from './components/Dashboard'; 
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setToken('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={ApolloClient}>
+      <div>
+        {token ? (
+          <>
+            <Dashboard onLogout={handleLogout} />
+          </>
+        ) : (
+          <Auth />
+        )}
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
