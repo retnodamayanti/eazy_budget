edc282efd82367156458848bba6a2f36206c0003
retnodamayanti <mayahere5@gmail.com>
2023-08-24 17:05:59 +0930

    added total expenses on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ExpenseList from './ExpenseList.js';
+import { useQuery } from '@apollo/client';
+import { GET_EXPENSES } from '../utils/queries';
+
 
 const Dashboard = ({ user }) => {
+  const { loading, error, data } = useQuery(GET_EXPENSES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const expenses = data.expenses;
+  const totalExpense = expenses.reduce((acc, expense) => acc + expense.amount, 0);
+
   return (
     <div className="dashboard">
       <h2>Welcome, {user?.username || 'Guest'}!</h2>
-      <ExpenseList />
+      <h3>Total Expense: ${totalExpense}</h3>
+      <ExpenseList expenses={expenses} />
     </div>
   );
 };
