7738a8e324c589f77a3b3c39c02a7fcfbdbf6c77
retnodamayanti <mayahere5@gmail.com>
2023-08-24 14:24:30 +0930

    added ExpenseList components

---
+++ client/src/components/ExpenseList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_EXPENSES } from '../utils/queries'; 
+
+const ExpenseList = () => {
+  const { loading, error, data } = useQuery(GET_EXPENSES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const expenses = data.expenses;
+
+  return (
+    <div className="expense-list">
+      <h3>Expense List</h3>
+      <ul>
+        {expenses.map(expense => (
+          <li key={expense._id}>
+            <div>Description: {expense.description}</div>
+            <div>Amount: {expense.amount}</div>
+            <div>Category: {expense.category}</div>
+            <div>Date: {expense.date}</div>
+            <div className="expense-buttons">
+              <button onClick={() => handleUpdateExpense(expense)}>Update</button>
+              <button onClick={() => handleRemoveExpense(expense._id)}>Remove</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleAddExpense}>Add Expense</button>
+    </div>
+  );
+};
+
+export default ExpenseList;
