9318fc47a4987879f806db15e6b9ad3bedb05924
retnodamayanti <mayahere5@gmail.com>
2023-08-24 17:07:56 +0930

    imported NavBar, ExpenseManager, and SignUp components

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,28 @@ import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import NavBar from './components/NavBar';
+import ExpenseManager from './components/ExpenseManager';
+import Signup from './components/Signup';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [user, setUser] = useState(null); // Store the user data
+  const [user, setUser] = useState(null);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+    setLoggedIn(false);
+  };
 
   return (
     <div className="App">
+      {loggedIn && <NavBar onLogout={handleLogout} />}
       <Routes>
         <Route path="/" element={<Login setLoggedIn={setLoggedIn} setUser={setUser} />} />
         <Route path="/dashboard" element={loggedIn ? <Dashboard user={user} /> : <Navigate to="/" />} />
+        <Route path="/expense" element={loggedIn ? <ExpenseManager /> : <Navigate to="/" />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
