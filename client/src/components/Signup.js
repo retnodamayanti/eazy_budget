b034a9a6285739e44abef2164b58bc5eb3327169
retnodamayanti <mayahere5@gmail.com>
2023-08-24 16:25:07 +0930

    added username filed

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { SIGNUP } from '../utils/mutations';
+import { useHistory } from 'react-router-dom'; // Import useHistory
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState(''); // Added username state
 
   const [signupMutation, { loading, error }] = useMutation(SIGNUP);
+  const history = useHistory(); // Use the useHistory hook
 
-  const handleSignup = () => {
-    signupMutation({ variables: { email, password } });
+  const handleSignup = async () => {
+    try {
+      await signupMutation({ variables: { username, email, password } });
+      history.push('/login'); // Redirect to login page after successful signup
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
   };
 
   return (
@@ -17,6 +25,15 @@ const Signup = () => {
       <h2>Signup</h2>
       {error && <p>Error: {error.message}</p>}
       <form>
+        {/* Added input for username */}
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
         <div>
           <label>Email:</label>
           <input
