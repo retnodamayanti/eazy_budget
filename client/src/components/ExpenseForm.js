2670fba684723f49b85047d6f4d510e94bcf3fde
retnodamayanti <mayahere5@gmail.com>
2023-08-25 02:10:31 +0930

    added ExpenseForm to manage update and addexpenses in dashboard

---
+++ client/src/components/ExpenseForm.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const ExpenseForm = ({ mode, initialExpense, onSubmit, onClose }) => {
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState('');
+  const [date, setDate] = useState(new Date());
+
+
+  useEffect(() => {
+    if (mode === 'update' && initialExpense) {
+      setDescription(initialExpense.description);
+      setAmount(initialExpense.amount);
+      setCategory(initialExpense.category);
+    }
+  }, [mode, initialExpense]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({
+      description,
+      amount: Number(amount),
+      category,
+      date: date.toISOString()
+    });
+    onClose();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      />
+      <DatePicker 
+        selected={date} 
+        onChange={date => setDate(date)} 
+        dateFormat="yyyy-MM-dd"
+      />
+      <button type="submit">{mode === 'add' ? 'Add' : 'Update'} Expense</button>
+      <button type="button" onClick={onClose}>Cancel</button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
--- client/src/components/ExpenseList.js
+++ client/src/components/ExpenseList.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_EXPENSES } from '../utils/queries'; 
 import { useHandleAddExpense, useHandleUpdateExpense, useHandleRemoveExpense } from '../utils/expenseHandlers';
-
+import ExpenseForm from './ExpenseForm';
 
 const ExpenseList = () => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [formMode, setFormMode] = useState('add'); // 'add' or 'update'
+  const [currentExpense, setCurrentExpense] = useState(null); // For updating
+
   const handleAddExpense = useHandleAddExpense();
   const handleUpdateExpense = useHandleUpdateExpense();
   const handleRemoveExpense = useHandleRemoveExpense();
-  const { loading, error, data } = useQuery(GET_EXPENSES, {
-    fetchPolicy: "network-only"
-  });
   
+  const { loading, error, data } = useQuery(GET_EXPENSES);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   const expenses = data.expenses;
 
+  const showAddForm = () => {
+    setFormMode('add');
+    setCurrentExpense(null);
+    setIsFormVisible(true);
+  };
+
+  const showUpdateForm = (expense) => {
+    setFormMode('update');
+    setCurrentExpense(expense);
+    setIsFormVisible(true);
+  };
+
+  const closeForm = () => {
+    setIsFormVisible(false);
+    setCurrentExpense(null);
+  };
+
+  const handleAddFormSubmit = (expenseDetails) => {
+    handleAddExpense(expenseDetails);
+    closeForm();
+  };
+
+  const handleUpdateFormSubmit = (expenseDetails) => {
+    if (currentExpense) {
+      handleUpdateExpense(currentExpense._id, expenseDetails);
+      closeForm();
+    }
+  };
+
   return (
     <div className="expense-list">
       <h3>Expense List</h3>
+      {isFormVisible && (
+        <div>
+          <h4>{formMode === 'add' ? 'Add Expense' : 'Update Expense'}</h4>
+          <ExpenseForm
+            mode={formMode}
+            initialExpense={currentExpense}
+            onSubmit={formMode === 'add' ? handleAddFormSubmit : handleUpdateFormSubmit}
+            onClose={closeForm}
+          />
+        </div>
+      )}
       <ul>
         {expenses.map(expense => (
           <li key={expense._id}>
@@ -29,13 +71,13 @@ const ExpenseList = () => {
             <div>Category: {expense.category}</div>
             <div>Date: {expense.date}</div>
             <div className="expense-buttons">
-              <button onClick={() => handleUpdateExpense(expense)}>Update</button>
+              <button onClick={() => showUpdateForm(expense)}>Update</button>
               <button onClick={() => handleRemoveExpense(expense._id)}>Remove</button>
             </div>
           </li>
         ))}
       </ul>
-      <button onClick={handleAddExpense}>Add Expense</button>
+      <button onClick={showAddForm}>Add Expense</button>
     </div>
   );
 };
