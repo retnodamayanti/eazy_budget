e1b8c1eb73d1735d392c629b8433da08b627c027
retnodamayanti <mayahere5@gmail.com>
2023-08-24 14:23:43 +0930

    added signup and add, remove, update mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,19 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_MUTATION = gql`
+export const SIGNUP = gql`
+  mutation Signup($username: String!, $email: String!, $password: String!) {
+    signup(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
   mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
@@ -9,6 +22,42 @@ export const LOGIN_MUTATION = gql`
         username
         email
       }
+    }
+  }
+`;
+
+export const ADD_EXPENSE = gql`
+  mutation AddExpense($input: ExpenseInput!) {
+    addExpense(input: $input) {
+      _id
+      description
+      amount
+      category
+      date
+    }
+  }
+`;
+
+export const UPDATE_EXPENSE = gql`
+  mutation UpdateExpense($expenseId: ID!, $input: ExpenseInput!) {
+    updateExpense(expenseId: $expenseId, input: $input) {
+      _id
+      description
+      amount
+      category
+      date
+    }
+  }
+`;
+
+export const REMOVE_EXPENSE = gql`
+  mutation RemoveExpense($expenseId: ID!) {
+    removeExpense(expenseId: $expenseId) {
+      _id
+      description
+      amount
+      category
+      date
     }
   }
 `;
