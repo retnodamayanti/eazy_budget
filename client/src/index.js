3fcc2e287cff010f75efe1248f297a95ed0f7b7e
retnodamayanti <mayahere5@gmail.com>
2023-08-26 10:13:26 +0930

    imported bootstrap in client/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from '@apollo/client'; // Import ApolloProvider
-import client from './utils/ApolloClient'; // Import your Apollo Client instance
+import { ApolloProvider } from '@apollo/client'; 
+import client from './utils/ApolloClient'; 
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './global.css';
 
 ReactDOM.render(
     <ApolloProvider client={client}>
